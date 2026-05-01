7b472a88e76394ff44060e0246804445d1b3fba5
Sabbir Ali <128308885+Sabbir45ali@users.noreply.github.com>
2026-05-01 23:26:55 +0530

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -6,7 +6,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
